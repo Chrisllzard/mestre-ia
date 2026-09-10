84343b49998610f352e2c2878a92f2c1f0e70ecb
Chrisllzard <ctoldra@xtec.cat>
2026-09-10 10:51:49 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ interface SAData {
 // Funció per generar la Situació d'Aprenentatge amb Gemini
 const generateSA = async (promptText: string): Promise<SAData> => {
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
